32a4a980a799f975466189f5e9b8e0799a1387ff
Arabella <127732263+ArabellaJordan@users.noreply.github.com>
2023-07-25 06:06:29 +0800

    labels

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //Keypress===========
 for (var i=0; i<19; i++){
+    document.getElementsByClassName("white")[i].addEventListener("click", keyClick);
     document.getElementsByClassName("white")[i].addEventListener("keypress", keyPress);
 }
 
+function keyClick(){
+    alert("clicked!");
+}
 function keyPress(event){
     alert(event.key);
 }
